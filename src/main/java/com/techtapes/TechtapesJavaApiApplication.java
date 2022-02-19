413546aac997636c94bc052d1da4ca4a1f206a5c
NuminousBlue <jdiehl2236@gmail.com>
2022-02-19 04:54:39 -0600

    initial commit

---
+++ src/main/java/com/techtapes/TechtapesJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.techtapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtapesJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtapesJavaApiApplication.class, args);
+	}
+
+}
